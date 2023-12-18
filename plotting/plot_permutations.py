a329fda557248df714d907791a0a9d117f77aa8b
strasserle <strasser.lena@gmx.de>
2023-12-18 12:18:16 +0100

    additional ns, normal knn metric

--- plotting/plot_permutations.py
+++ plotting/plot_permutations.py
@@ -16,12 +16,22 @@ wd = os.getcwd()
 data_dir = 'D:\\11. Semester\\MA\\benchmarking\\data\\test_data\\permut_data'
 fig_dir = "plotting/figures/"
 permut_dir = "D:/11. Semester/MA/"
-results_dir = "results"
+results_dir = "results_run2"
+plot_data_dir = "plotting/plot_data"
 
 # dataset = "bone_marrow"
 dataset = "liver_four"
 p, metrics = get_results_of_batch(f"eval_on_full_{dataset}", results_dir=results_dir, permut_dir=permut_dir)
 eval_on_one, _ = get_results_of_batch(f"eval_on_one_{dataset}", results_dir=results_dir, permut_dir=permut_dir)
+
+# save
+p.write_csv(os.path.join(plot_data_dir, f"permutations_{dataset}.csv"))
+print(metrics)
+eval_on_one.to_csv(os.path.join(plot_data_dir, f"eval_on_one_{dataset}.csv"))
+
+# p = pd.read_csv(os.path.join(plot_data_dir, f"permutations_{dataset}"))
+# metrics = {'forest_clfs accuracy', 'forest_clfs perct acc > 0.8'}
+# eval_on_one = pd.read_csv(os.path.join(f"eval_on_one_{dataset}.txt"))
 
 plt.style.use("fivethirtyeight")
 p.sort_values(by=["n_datasets"], inplace=True)
@@ -62,7 +72,7 @@ for metric, ax, color in zip(metrics, axes, prop_cycle.by_key()['color']):
     # plt.plot(p["n_datasets"], p[metric], "x", markersize=10, label=f"{metric} on full dataset")
     # plt.plot(p["n_datasets"].unique(), p[["n_datasets", metric]].groupby(by="n_datasets").mean(), "-x", markersize=10, label=f"{metric} mean over selections", color=color)
     ax.scatter(mean_eval["n_datasets"], mean_eval[metric], s=minmax_norm(mean_eval["n_cells"], min=10, max=40), label=f"{metric} per selection", color=color)
-    ax.plot(mean_eval["n_datasets"].unique(), mean_eval.groupby("n_datasets").mean()[metric], "--o", markersize=10, label=f"{metric} mean over selections", color=color)
+    ax.plot(mean_eval["n_datasets"].unique(), mean_eval.groupby("n_datasets").mean()[metric], "--x", markersize=15, label=f"{metric} mean over selections", color=color)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.set_title(metric)
 # legend_1 = axes[0].legend(loc='upper center', bbox_to_anchor=(0.5, -0.15))
@@ -78,8 +88,8 @@ legend_2 = ax_2.legend(handles=legend_proxies, title='Number of cells', loc='upp
 ax_3 = ax_2.twiny()
 ax_3.set_axis_off()
 # third legend: one grey dot saying "score per selection" and one dotted line with a dot saying "mean over selections" also in grey, below legend_2
-legend_3 = ax_3.legend(handles=[plt.Line2D([0], [0], marker='o', color='grey', markerfacecolor='grey', markersize=10, label="score per selection"),
-                                plt.Line2D([0], [0], marker='o', color='grey', markerfacecolor='grey', markersize=10, linestyle="--", label="mean over selections")], loc='upper left', bbox_to_anchor=(1.05, 0.5))
+legend_3 = ax_3.legend(handles=[plt.Line2D([0], [0], marker='o', color='white', markerfacecolor='grey', markersize=10, label="score per selection"),
+                                plt.Line2D([0], [0], marker='X', markersize=10, color='grey', markerfacecolor='grey', markersize=10, linestyle="--", label="mean over selections")], loc='upper left', bbox_to_anchor=(1.05, 0.5))
 # suptitle in same fontsize as axes titles
 plt.suptitle(f"Dataset: {dataset}", x=0.4, fontsize=ax.title.get_fontsize())
 fig.text(0.4, 0.03, 'Number of datasets in permutation', ha='center', va='center')
